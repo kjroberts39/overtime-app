dab408b04e808d3d2d748ad330b2977d2b3801ed
KJ Roberts <kjroberts39@hotmail.com>
2017-03-23 14:38:54 -0600

    Added delete link and functionality to each post, or post partial

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-before_action :set_post, only: [:show, :edit, :update]
+before_action :set_post, only: [:show, :edit, :update, :destroy]
 	def index
 		@posts = Post.all
 	end
@@ -31,6 +31,11 @@ before_action :set_post, only: [:show, :edit, :update]
 		else
 			render :edit
 		end
+	end
+
+	def destroy
+	  @post.delete
+	  redirect_to posts_path, notice: 'Your post was deleted successfully'
 	end
 
 private
