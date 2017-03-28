58d0d9eb2e5f2659573dd833383b16eff51cf276
KJ Roberts <kjroberts39@hotmail.com>
2017-03-28 15:56:01 -0600

    implemented post policy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,9 +23,12 @@ before_action :set_post, only: [:show, :edit, :update, :destroy]
 	end
 
 	def edit
+		authorize @post
 	end
 
 	def update
+		authorize @post
+		
 		if @post.update(post_params)
 			redirect_to @post, notice: 'Your post was edited successfully'
 		else
