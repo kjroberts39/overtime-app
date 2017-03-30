45dae20a3f5e7475911adc766745eeb1b94bb410
KJ Roberts <kjroberts39@hotmail.com>
2017-03-30 15:17:05 -0600

    added overtime request for hours in model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -44,7 +44,7 @@ before_action :set_post, only: [:show, :edit, :update, :destroy]
 private
 
   def post_params
-  	params.require(:post).permit(:date, :rationale, :status)
+  	params.require(:post).permit(:date, :rationale, :status, :overtime_request)
   end
 
   def set_post
